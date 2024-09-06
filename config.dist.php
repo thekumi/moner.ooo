a45ca6b2b6322328a6e3775a02995839cc947324
rottenwheel <92872541+rottenwheel@users.noreply.github.com>
2024-09-06 19:37:44 +0000

    Update config.dist.php

--- config.dist.php
+++ config.dist.php
@@ -2,13 +2,13 @@
 return [
     'attribution' => '', // Custom attribution HTML to show in the info text
     'footer_links' => [ // Custom links to show in the footer
-        ['text' => 'Clearnet', 'url' => 'https://monerooo.private.coffee/'],
-        ['text' => 'Tor', 'url' => 'http://monerooo.coffee2m3bjsrrqqycx6ghkxrnejl2q6nl7pjw2j4clchjj6uk5zozad.onion'],
-        ['text' => 'Private.coffee', 'url' => 'https://private.coffee']
+        ['text' => 'Clearnet', 'url' => 'https://calc.revuo-xmr.com'],
+        ['text' => 'Tor', 'url' => 'http://calc.revuo75joezkbeitqmas4ab6spbrkr4vzbhjmeuv75ovrfqfp47mtjid.onion'],
+        ['text' => 'Revuo Monero', 'url' => 'https://www.revuo-xmr.com/']
     ],
     'preferred_currencies' => [ // Currencies that should be displayed at the top of the lists
         'usd', 'eur', 'gbp', 'cad', 'btc', 'eth', 'ltc'
     ],
-    'github_url' => 'https://git.private.coffee/kumi/moner.ooo/', // URL to the GitHub repository - replace if you forked the project
+    'github_url' => 'https://github.com/rottenwheel/moner.ooo/', // URL to the GitHub repository - replace if you forked the project
     'servers_guru' => false, // Show the "Servers Guru" attribution link in the info text - here for upstream compatibility
 ];
