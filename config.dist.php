814772b5714990e0c9e4dd47244381beedc4cca1
Kumi <git@kumi.email>
2024-09-06 13:25:16 +0200

    chore(config): update URLs and attribution settings in config example
    Updated various URLs including clearnet, Tor, and Git repository links to reflect the new domain changes. Added a new 'Private.coffee' link to footer links.

--- config.dist.php
+++ config.dist.php
@@ -1,13 +1,14 @@
 <?php
 return [
-    'servers_guru' => true, // Show the "Servers Guru" attribution link in the info text
     'attribution' => '', // Custom attribution HTML to show in the info text
     'footer_links' => [ // Custom links to show in the footer
-        ['text' => 'Clearnet', 'url' => 'https://calc.revuo-xmr.com'],
-        ['text' => 'Tor', 'url' => 'http://calc.revuo75joezkbeitqmas4ab6spbrkr4vzbhjmeuv75ovrfqfp47mtjid.onion']
+        ['text' => 'Clearnet', 'url' => 'https://monerooo.private.coffee/'],
+        ['text' => 'Tor', 'url' => 'http://monerooo.coffee2m3bjsrrqqycx6ghkxrnejl2q6nl7pjw2j4clchjj6uk5zozad.onion'],
+        ['text' => 'Private.coffee', 'url' => 'https://private.coffee']
     ],
     'preferred_currencies' => [ // Currencies that should be displayed at the top of the lists
         'usd', 'eur', 'gbp', 'cad', 'btc', 'eth', 'ltc'
     ],
-    'github_url' => 'https://github.com/rottenwheel/moner.ooo/',
+    'github_url' => 'https://git.private.coffee/kumi/moner.ooo/', // URL to the GitHub repository - replace if you forked the project
+    'servers_guru' => false, // Show the "Servers Guru" attribution link in the info text - here for upstream compatibility
 ];
